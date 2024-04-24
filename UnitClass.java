fac31d5d6b8fe9a9c08a6741f4148c84f2cd405a
Alex Taraba <161524033+Artaraba@users.noreply.github.com>
2024-04-24 08:22:50 -0500

    Update UnitClass.java

--- UnitClass.java
+++ UnitClass.java
@@ -1,3 +1,15 @@
 // This program will create a unit when requested by the user
 
 import java.util.Scanner;
+
+class UnitClass {
+
+  String unitName = "";
+  double hp = 0, attack = 0, defense = 0;
+
+  Worker() {
+    unitName = "Worker";
+    hp = 50;
+    attack = 1;
+    defense = 0;
+}
