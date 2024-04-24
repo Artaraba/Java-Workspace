48af06f85c91b3d069ba8a339119b1a1675aedc8
Alex Taraba <161524033+Artaraba@users.noreply.github.com>
2024-04-24 17:18:25 -0500

    Update and rename TestUnit.java to TestRobot.java
    Also implemented new code to create robot objects.

---
+++ TestRobot.java
@@ -0,0 +1,15 @@
+package robot;
+
+public class TestRobot {
+
+	public static void main(String[] args) {
+		
+		Robot codey = new Robot("Codey", "Worker");
+		System.out.print(codey.getName());
+		System.out.println();
+		System.out.print(codey.getType());
+		
+
+	}
+
+}
