b915afefd100fcdae6eeac405bd952a881e316ae
Artaraba <161524033+Artaraba@users.noreply.github.com>
2024-04-24 10:01:59 -0500

    Corrected block

--- UnitClass.java
+++ UnitClass.java
@@ -7,9 +7,10 @@ class UnitClass {
   String unitName = "";
   double hp = 0, attack = 0, defense = 0;
 
-  Worker() {
-    unitName = "Worker";
-    hp = 50;
-    attack = 1;
-    defense = 0;
+	Worker() {
+		unitName = "Worker";
+		hp = 50;
+		attack = 1;
+		defense = 0;
+	}
 }
