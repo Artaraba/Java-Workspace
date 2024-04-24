d6734c8eca5db56cdf4b2033bd4819723c2312f4
Alex Taraba <161524033+Artaraba@users.noreply.github.com>
2024-04-24 10:31:30 -0500

    Update testUnitClass.java
    Rewrite to test UnitClass

--- testUnitClass.java
+++ testUnitClass.java
@@ -1,18 +1,15 @@
-// This is a program to test the UnitClass.java file
+package unit;
 
 import java.util.Scanner;
 
-public class testUnitClass {
-		public static void main(String[] args) {
-			System.out.println("What type of unit would you like to create?");
-			System.out.print("Worker, Fighter, Mage, Elite: ");
-			String unitType = input.next();
-			
-			if (unitType = "Worker") {
-				Worker worker = new Worker();
-			}
-			
-			System.out.print(worker.unitName, + "," + worker.hp + "," + worker.attack + "," + worker.defense);
-		}
-	
+public class TestUnit {
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("What type of unit do you wish to create?");
+		String unitType = input.next();
+		System.out.print(unitType);
+		Unit unitOne = new Unit(unitType);
+	}
 }
