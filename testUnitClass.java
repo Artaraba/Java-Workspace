438b17a054fc5e1358a7fa657645348727ccf194
Artaraba <161524033+Artaraba@users.noreply.github.com>
2024-04-24 08:35:43 -0500

    Update testUnitClass.java
    Added text for user to select unit to build. Only Worker object has been defined.

--- testUnitClass.java
+++ testUnitClass.java
@@ -1,1 +1,18 @@
 // This is a program to test the UnitClass.java file
+
+import java.util.Scanner;
+
+public class testUnitClass {
+		public static void main(String[] args) {
+			System.out.println("What type of unit would you like to create?");
+			System.out.print("Worker, Fighter, Mage, Elite: ");
+			String unitType = input.next();
+			
+			if (unitType = "Worker") {
+				Worker worker = new Worker();
+			}
+			
+			System.out.print(worker.unitName, + "," + worker.hp + "," + worker.attack + "," + worker.defense);
+		}
+	
+}
