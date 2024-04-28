ebca603f7840f64cfd2fe95b39f096e6f9a2a374
Alex Taraba <161524033+Artaraba@users.noreply.github.com>
2024-04-27 20:24:34 -0500

    Update TestRobot.java
    Updated to print all stats for each unit created

--- TestRobot.java
+++ TestRobot.java
@@ -8,6 +8,25 @@ public class TestRobot {
 		System.out.print(codey.getName());
 		System.out.println();
 		System.out.print(codey.getType());
+		System.out.println();
+		System.out.print(codey.getHp());
+		System.out.println();
+		System.out.print(codey.getAttack());
+		System.out.println();
+		System.out.println(codey.getDefense());
+		System.out.println();
+		
+		Robot jeffrey = new Robot("Jeffrey", "Fighter");
+		System.out.print(jeffrey.getName());
+		System.out.println();
+		System.out.print(jeffrey.getType());
+		System.out.println();
+		System.out.print(jeffrey.getHp());
+		System.out.println();
+		System.out.print(jeffrey.getAttack());
+		System.out.println();
+		System.out.println(jeffrey.getDefense());
+		System.out.println();
 		
 
 	}
